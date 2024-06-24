7065f6bb70031fa1db8f2376cefc9177337c1c26
SaidCarrollo <harvy.carrollo@tecsup.edu.pe>
2024-06-23 21:09:36 -0400

    Luces

--- ProyectoFinal3erCiclo(Ariel)/Assets/Scripts/PlayerController.cs
+++ ProyectoFinal3erCiclo(Ariel)/Assets/Scripts/PlayerController.cs
@@ -7,21 +7,22 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] public Inventory inventory;
     [SerializeField] private float MovementVelocity;
-    [SerializeField] private Transform cameraTransform; 
+    [SerializeField] private Transform cameraTransform;
 
     private Animator anim;
     private Vector2 movementInput;
+    private bool isPickingUp = false; // Flag to track "Pick Up" animation state
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
-        cameraTransform = Camera.main.transform; 
+        cameraTransform = Camera.main.transform;
     }
 
     private void Update()
     {
         Vector3 cameraForward = cameraTransform.forward;
-        cameraForward.y = 0f; 
+        cameraForward.y = 0f;
 
         Quaternion targetRotation = Quaternion.LookRotation(cameraForward);
 
@@ -33,9 +34,16 @@ public class PlayerController : MonoBehaviour
 
         anim.SetInteger("VelX", (int)movementInput.x);
         anim.SetInteger("VelY", (int)movementInput.y);
-        if(movementInput.y == 1 && movementInput.x == 1)
+
+        // Check for animation completion using normalized time
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f && isPickingUp)
         {
-            anim.SetTrigger("Run");
+            isPickingUp = false; // Reset flag after animation finishes
+            anim.SetBool("Pick", false); // Deactivate "Pick" animation
+        }
+        if (isPickingUp)
+        {
+            movementInput = Vector2.zero; // Set movement input to zero
         }
     }
 
@@ -43,15 +51,34 @@ public class PlayerController : MonoBehaviour
     {
         movementInput = context.ReadValue<Vector2>();
     }
+
+    public void PickUp(InputAction.CallbackContext context)
+    {
+        if (context.performed && !isPickingUp) // Trigger animation only once
+        {
+            isPickingUp = true; // Set flag to track animation state
+            anim.SetBool("Pick", true);
+        }
+    }
+
+    public void Throw(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            // Implement throwing logic here
+        }
+    }
     void OnTriggerStay(Collider other)
     {
-        if (other.tag == "SafeObject" && Input.GetKeyDown(KeyCode.E))
+        if (other.tag == "SafeObject")
         {
             inventory.SaveObject(other.gameObject);
             other.gameObject.SetActive(false);
         }
     }
 }
+
+
 
 
 
